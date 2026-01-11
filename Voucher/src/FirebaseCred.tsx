925d371c21873d7a078c9784230b3497bf56b692
Puneeth Reddy Bogala <103556737+puneeththe01@users.noreply.github.com>
2026-01-11 15:47:43 +0800

    Obscure Firebase credentials in FirebaseCred
    Replaced sensitive Firebase credentials with placeholders.

--- Voucher/src/FirebaseCred.tsx
+++ Voucher/src/FirebaseCred.tsx
@@ -6,16 +6,17 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// below credentials are removed and replaced with '*'
 const firebaseConfig = {
-  apiKey: "AIzaSyDiqoCwdiWaQkUddiPAFN5jOtgf1hCCtqo",
-  authDomain: "no-title-6669e.firebaseapp.com",
+  apiKey: "*************************************",
+  authDomain: "**********.firebaseapp.com",
   databaseURL:
-    "https://no-title-6669e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "no-title-6669e",
-  storageBucket: "no-title-6669e.appspot.com",
-  messagingSenderId: "770850130178",
-  appId: "1:770850130178:web:95c8e9d8300099ee6f1a1b",
-  measurementId: "G-CC276L8673",
+    "*******************************************************************",
+  projectId: "no-title-*****",
+  storageBucket: "no-title-**************",
+  messagingSenderId: "***************",
+  appId: "*************************************************",
+  measurementId: "G-**********",
 };
 
 // Initialize Firebase
